9132a89522649d888a401065da86f7471c4be563
Mateo Manolio <mateo.manolio@globant.com>
2023-11-03 15:28:39 -0300

    first implementation of the hero animation between pages. Only is left the Carrousel

--- lib/src/presentation/navigation/movie_details_args.dart
+++ lib/src/presentation/navigation/movie_details_args.dart
@@ -3,9 +3,13 @@ import '../../domain/entity/movie.dart';
 class MovieDetailsArguments {
   final Movie movie;
   final Function(Movie) setLastMovie;
+  final String posterTag;
+  final String backdropTag;
 
   MovieDetailsArguments({
     required this.movie,
     required this.setLastMovie,
+    required this.backdropTag,
+    required this.posterTag,
   });
 }
